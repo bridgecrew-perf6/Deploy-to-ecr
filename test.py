5e19df552b42f55d44e49935dcc90486790f2af5
ilyar1988 <104307060+ilyar1988@users.noreply.github.com>
2022-05-17 10:25:57 +0300

    Change import docker to from requests import get.

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-import docker
+from requests import get
 
-client = docker.DockerClient()
-container = client.containers.get("ubuntu20.04")
-ip_add = container.attrs['NetworkSettings']['IPAddress']
-
+myip = get('https://l2.io/ip').text
+print ('My IP address is: ', myip)
 my_file = open("Find ip.html", "w")
 text_list = ["<html>", "<body>"]
-text_list.append(ip_add)
+text_list.append(myip)
 text_list.append("</body>")
 text_list.append("</html>")
 my_file.writelines(text_list)
