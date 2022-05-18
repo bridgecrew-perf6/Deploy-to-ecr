315cd36a7f70ef002e18fb4016378f4a71a5dcd7
ilyar1988 <104307060+ilyar1988@users.noreply.github.com>
2022-05-18 16:56:17 +0300

    Update git_update.sh

--- build/git_update.sh
+++ build/git_update.sh
@@ -10,13 +10,13 @@ do
   esac
 done
 
-# get highest tag number, and add v0.1.0 if doesn't exist
+# get highest tag number, and add v0.1 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v0.1'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
