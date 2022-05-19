1fe8804c578080a3d3212bacc279742d336cd508
ilyar1988 <104307060+ilyar1988@users.noreply.github.com>
2022-05-19 12:24:00 +0300

    Update git_update.sh

--- build/git_update.sh
+++ build/git_update.sh
@@ -12,6 +12,8 @@ echo "Current Version: $CURRENT_VERSION"
 
 $NEW_TAG = $(echo CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 
+echo "New Tag: $NEW_TAG"
+
 git tag $NEW_TAG
 git push --tags
 git push
